d134c60b453e9d1f4622992bbe99dd70b4edc852
johnnychen85 <chz0321@gmail.com>
2016-12-15 14:17:39 +0800

    代码结构调整

--- AmazonAGCOD.php
+++ AmazonAGCOD.php
@@ -1,175 +1,167 @@
 <?php
+
 /**
  * @author Johnny Chen <chz0321@gmail.com>
  * @desc This class is for making a request to Amazon GiftCode on Demand service API.
  *       amazon agcod official doc: https://s3.amazonaws.com/AGCOD/tech_spec/AGCODTechSpec.pdf
  *
  * sample code:
- * 
+ *
  * $partnerId = 'YourCompanyID';
  * $accessKey = 'findfromYourAwsAccountManagementPage';
  * $secretKey = 'findYourAwsAccountManagementPage';
- * $regionCode = 'us-east-1';
+ * $regionCode = 'us-east-1'; //your aws server region
  * $host = 'host:agcod-v2-gamma.amazon.com';
  * $endpoint = 'host:agcod-v2-gamma.amazon.com';
  *
  * $uniqueRequestId = $partnerId.rand(0,99); //assign a unique request id for each request
- * 
+ *
  * $gcod = new AmazonGCOD($partnerId,$accessKey,$secretKey,$regionCode,$host,$endpoint,$uniqueRequestId);
  * $giftcard = $gcod->createGiftCode(5); //request for a USD$5 giftcard code
  *
- * $gcod->cancelGiftCode($giftcard['gcId']); //
+ * $gcod->cancelGiftCode($giftcard['gcId']); //cancel the code by Code ID
  */
+class AmazonGCOD
+{
+    protected $_partnerId = '';
+    protected $_accessKey = '';
+    protected $_secretKey = '';
+    protected $_regionCode = '';
+    protected $_service = 'AGCODService';
+    protected $_endpoint = '';
+    protected $_host = '';
 
-class AmazonGCOD{	
-	private $a_PartnerId = '';
-	private $a_AccessKey = '';
-	private $a_SecretKey = '';
-	private $a_RegionCode = '';
-	private $a_ServiceName = 'AGCODService';
-	private $a_Endpoint = '';
-	private $a_Host = '';
-	
-	private $a_Timestamp_ISO8601 = '';
-	private $a_Timestamp = '';
-	private $a_RequestId = '';
-	
-    public function __construct($partnerID,$accessKey,$secretKey,$regionCode,$host,$endpoint,$requestId)
-    {
-        $this->a_PartnerId = $partnerID;
-        $this->a_AccessKey = $accessKey;
-        $this->a_SecretKey = $secretKey;
-        $this->a_RegionCode = $regionCode;
-        $this->a_Host      = $host;
-        $this->a_Endpoint  = $endpoint;
+    protected $_timestampISO8601 = '';
+    protected $_timestamp = '';
+    protected $_requestId = '';
 
-    	$time = date('Ymd\THis\Z',time() - date('Z'));
-    	$this->a_Timestamp_ISO8601 = $time;
-    	$this->a_Timestamp = substr($time,0,8);
-    	
-    	$str = sprintf('%012s', $requestId);
-    	$this->a_RequestId = $this->a_PartnerId . $str;
+    public function __construct($partnerID, $accessKey, $secretKey, $regionCode, $host, $endpoint, $requestId) {
+        $this->_partnerId  = $partnerID;
+        $this->_accessKey  = $accessKey;
+        $this->_secretKey  = $secretKey;
+        $this->_regionCode = $regionCode;
+        $this->_host       = $host;
+        $this->_endpoint   = $endpoint;
+
+        $time                    = date('Ymd\THis\Z', time() - date('Z'));
+        $this->_timestampISO8601 = $time;
+        $this->_timestamp        = substr($time, 0, 8);
+
+        $str              = sprintf('%012s', $requestId);
+        $this->_requestId = $this->_partnerId . $str;
     }
-    
+
     public function createGiftCode($gc_amount) {
         $op = 'CreateGiftCard';
         // step1. gen json "PAYLOAD"
-        $Data = array();
+        $Data = [];
 
-        $Data['creationRequestId'] = $this->a_RequestId;
-        $Data['partnerId'] = $this->a_PartnerId;
-        $Data['value'] = array('currencyCode' => 'USD', 'amount' => $gc_amount);
-        
-        $payload = json_encode($Data);
+        $Data['creationRequestId'] = $this->_requestId;
+        $Data['partnerId']         = $this->_partnerId;
+        $Data['value']             = ['currencyCode' => 'USD', 'amount' => $gc_amount];
 
-    	$payloadHashed = $this->hashPayload($payload);
+        $payload                = json_encode($Data);
+        $payloadHashed          = $this->_hashPayload($payload);
+        $CanonicalRequestHashed = $this->_hashCanonicalRequest($payloadHashed, $op);
+        $signature              = $this->_generateSignature($CanonicalRequestHashed);
+        $return_content         = $this->_sendRequest($signature, $payload, $op);
 
-    	$CanonicalRequestHashed = $this->hashCanonicalRequest($payloadHashed,$op);
-
-    	$signature = $this->generateSignature($CanonicalRequestHashed);
-
-    	$return_content = $this->sendRequest($signature,$payload,$op);
-    	
         // agcod 获取结果
-        $gc = json_decode($return_content, true);
-    	
-    	// 返回数据预处理
-    	$return = array(
-    		'status' => 'FAILURE',
-    		'code' => '',
-    		'json' => $return_content,
-    	);
-    	
-    	// 确认OK
-		if($gc['status'] == 'SUCCESS') {
-			$return['status'] = $gc['status'];
-			$return['code'] = $gc['gcClaimCode'];
-		}elseif($gc['errorCode'] == 'F300' && $gc['errorType'] == 'InsufficientFunds'){
+        $gc = json_decode($return_content, TRUE);
+
+        // 返回数据预处理
+        $return = array(
+            'status' => 'FAILURE',
+            'code'   => '',
+            'json'   => $return_content,
+        );
+
+        // 确认OK
+        if ($gc['status'] == 'SUCCESS') {
+            $return['status'] = $gc['status'];
+            $return['code']   = $gc['gcClaimCode'];
+        } elseif ($gc['errorCode'] == 'F300' && $gc['errorType'] == 'InsufficientFunds') {
             //insufficientFund error occur should notice me
-        }else{
+        } else {
             //other error
         }
-    	
-    	return $return;
+
+        return $return;
     }
 
-    public function cancelGiftCode($amazonGcId){
+    public function cancelGiftCode($amazonGcId) {
         $op = 'CancelGiftCard';
 
-        $data = array();
-        $data['partnerId'] = $this->a_PartnerId;
-        $data['creationRequestId'] = $this->a_RequestId;
-        $data['gcId'] = $amazonGcId;
+        $data                      = array();
+        $data['partnerId']         = $this->_partnerId;
+        $data['creationRequestId'] = $this->_requestId;
+        $data['gcId']              = $amazonGcId;
 
-        $payload = json_encode($data);
+        $payload                = json_encode($data);
+        $payloadHashed          = $this->_hashPayload($payload);
+        $CanonicalRequestHashed = $this->_hashCanonicalRequest($payloadHashed, $op);
+        $signature              = $this->_generateSignature($CanonicalRequestHashed);
+        $return_content         = $this->_sendRequest($signature, $payload, $op);
 
-        $payloadHashed = $this->hashPayload($payload);
-
-        $CanonicalRequestHashed = $this->hashCanonicalRequest($payloadHashed,$op);
-
-        $signature = $this->generateSignature($CanonicalRequestHashed);
-
-        $return_content = $this->sendRequest($signature,$payload,$op);
-        
         // agcod 获取结果
-        $gc = json_decode($return_content, true);
+        $gc = json_decode($return_content, TRUE);
 
         return $gc;
     }
 
-    private function hashPayload($payload){
+    protected function _hashPayload($payload) {
         // step2. hash $payload
-        $payloadHashed = hash('sha256',$payload);
+        $payloadHashed = hash('sha256', $payload);
 
         return $payloadHashed;
     }
 
-    private function hashCanonicalRequest($payloadHashed,$op = 'CreateGiftCard'){
+    protected function _hashCanonicalRequest($payloadHashed, $op = 'CreateGiftCard') {
         // step3. gen string "CANONICAL REQUEST" with $payloadHashed
-        $CanonicalRequest = "POST\n/{$op}\n\naccept:application/json\ncontent-type:application/json\n".$this->a_Host."\nx-amz-date:".$this->a_Timestamp_ISO8601."\nx-amz-target:com.amazonaws.agcod.AGCODService.{$op}\n\naccept;content-type;host;x-amz-date;x-amz-target\n".$payloadHashed;
-
+        $CanonicalRequest = "POST\n/{$op}\n\naccept:application/json\ncontent-type:application/json\n" . $this->_host . "\nx-amz-date:" . $this->_timestampISO8601 . "\nx-amz-target:com.amazonaws.agcod.AGCODService.{$op}\n\naccept;content-type;host;x-amz-date;x-amz-target\n" . $payloadHashed;
         // step4. hash $CanonicalRequest
-        $CanonicalRequestHashed = hash('sha256',$CanonicalRequest);
+        $CanonicalRequestHashed = hash('sha256', $CanonicalRequest);
 
         return $CanonicalRequestHashed;
     }
 
-    private function generateSignature($CanonicalRequestHashed){
+    protected function _generateSignature($CanonicalRequestHashed) {
         // step5. gen string "SIGN" with $CanonicalRequestHashed
-        $string2sign = "AWS4-HMAC-SHA256\n".$this->a_Timestamp_ISO8601."\n".$this->a_Timestamp."/".$this->a_RegionCode."/".$this->a_ServiceName."/aws4_request\n".$CanonicalRequestHashed;
+        $string2sign = "AWS4-HMAC-SHA256\n" . $this->_timestampISO8601 . "\n" . $this->_timestamp . "/" . $this->_regionCode . "/" . $this->_service . "/aws4_request\n" . $CanonicalRequestHashed;
 
         // step6. make "SIGNING KEY"
-         
+
         //$kSecret = SECRET_KEY
-        $kDate = hash_hmac('sha256', $this->a_Timestamp, 'AWS4'.$this->a_SecretKey, true);
-        $kRegion = hash_hmac('sha256', $this->a_RegionCode, $kDate, true);
-        $kService = hash_hmac('sha256', $this->a_ServiceName, $kRegion, true);
-        $kSigning = hash_hmac('sha256', 'aws4_request', $kService, true);
-        
+        $kDate    = hash_hmac('sha256', $this->_timestamp, 'AWS4' . $this->_secretKey, TRUE);
+        $kRegion  = hash_hmac('sha256', $this->_regionCode, $kDate, TRUE);
+        $kService = hash_hmac('sha256', $this->_service, $kRegion, TRUE);
+        $kSigning = hash_hmac('sha256', 'aws4_request', $kService, TRUE);
+
         // step7. "SIGNATURE" with $kSigning
         $signature = hash_hmac('sha256', $string2sign, $kSigning);
 
         return $signature;
     }
 
-    private function sendRequest($signature,$payload,$op = 'CreateGiftCard'){
+    protected function _sendRequest($signature, $payload, $op = 'CreateGiftCard') {
         $curl_head = array(
-        'accept:application/json',
-        'content-type:application/json',
-        $this->a_Host,
-        'x-amz-date:'.$this->a_Timestamp_ISO8601,
-        'x-amz-target:com.amazonaws.agcod.AGCODService.'.$op,
-        'Authorization:AWS4-HMAC-SHA256 Credential='.$this->a_AccessKey.'/'.$this->a_Timestamp.'/us-east-1/AGCODService/aws4_request, SignedHeaders=accept;content-type;host;x-amz-date;x-amz-target,Signature='.$signature,
+            'accept:application/json',
+            'content-type:application/json',
+            $this->_host,
+            'x-amz-date:' . $this->_timestampISO8601,
+            'x-amz-target:com.amazonaws.agcod.AGCODService.' . $op,
+            'Authorization:AWS4-HMAC-SHA256 Credential=' . $this->_accessKey . '/' . $this->_timestamp . '/us-east-1/AGCODService/aws4_request, SignedHeaders=accept;content-type;host;x-amz-date;x-amz-target,Signature=' . $signature,
         );
 
         // get gc
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->a_Endpoint.$op);
-        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $this->_endpoint . $op);
+        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_head);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
         $return_content = curl_exec($ch);
 
         curl_close($ch);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# amazon.agcod
+#AGCOD means Amazon Gift Code On Demand.
+
+
+Usage:
+```php
+$partnerId = 'YourCompanyID';
+$accessKey = 'findfromYourAwsAccountManagementPage';
+$secretKey = 'findYourAwsAccountManagementPage';
+$regionCode = 'us-east-1';
+$host = 'host:agcod-v2-gamma.amazon.com';
+$endpoint = 'host:agcod-v2-gamma.amazon.com';
+
+$uniqueRequestId = $partnerId.rand(0,99); //assign a unique request id for each request
+
+$gcod = new AmazonGCOD($partnerId,$accessKey,$secretKey,$regionCode,$host,$endpoint,$uniqueRequestId);
+$giftcard = $gcod->createGiftCode(5); //request for a USD$5 giftcard code
+
+$gcod->cancelGiftCode($giftcard['gcId']); //
+```
